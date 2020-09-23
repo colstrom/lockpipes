8d57a63d19eee7d3312d654104a28b635abedf04
Chris Olstrom <chris@olstrom.com>
2020-09-23 11:20:34 -0700

    lib - adds pub struct LockPipe and pub fn LockPipe::new(path)
    The LockPipe struct is the low-level interface for lockpipes, providing the core
    functionality, with minimal error handling.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+use std::path::PathBuf;
 
+#[derive(Debug)]
+pub struct LockPipe {
+  path: PathBuf,
+}
+
+impl LockPipe {
+  pub fn new<P: Into<PathBuf>>(path: P) -> Self {
+    Self { path: path.into() }
+  }
+}
